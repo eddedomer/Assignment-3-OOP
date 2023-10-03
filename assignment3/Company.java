17ae6b61447cfad929eca6fa4c32526a0e6ec312
eddedomer <31513157+eddedomer@users.noreply.github.com>
2023-10-03 15:02:54 +0200

    Update Company.java

--- assignment3/Company.java
+++ assignment3/Company.java
@@ -1,5 +1,4 @@
 package assignment3;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Company {
@@ -12,10 +11,17 @@ public class Company {
     public String createEmployee(String employeeID, String name, double grossSalary){
         String message = "";
         if (!(employeeID.isEmpty() || name.isEmpty())){
-            allEmployees.put(employeeID,new Employee(employeeID, name, grossSalary));
+            allEmployees.put(employeeID, new Employee(employeeID, name, grossSalary));
             message = String.format("Employee %s was registered successfully.", employeeID);
         }
         return message;
+    }
+    public String removeEmployee(String employeeID){
+        if(allEmployees.get(employeeID) != null){
+            allEmployees.remove(employeeID);
+            return String.format("Employee %s was successfully removed.", employeeID);
+        }
+        return  null;
     }
     public String printEmployee(String employeeID){
         return "" + allEmployees.get(employeeID);
@@ -23,4 +29,5 @@ public class Company {
     public double getNetSalary(String employeeID){
         return allEmployees.get(employeeID).getNetSalary();
     }
+
 }
